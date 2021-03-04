b59a15b58852e411a0e36e796448fa3476229d8b
LiamBartolini <liambartolini@gmail.com>
2021-03-04 08:38:35 +0100

    traduzione e commento

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,52 +6,48 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Counter c = new Counter(new Random().Next(10));
+            Counter c = new Counter(10); // Imposto il valore da raggiungere
             c.ThresholdReached += c_ThresholdReached;
 
-            Console.WriteLine("premi a per aumentare il totale...");
+            Console.WriteLine("premi 'a' per aumentare il totale...");
             while (Console.ReadKey(true).KeyChar == 'a')
             {
-                Console.WriteLine("adding one");
+                Console.WriteLine("aggiunto uno al totale");
                 c.Add(1);
             }
         }
 
         static void c_ThresholdReached(object sender, EventArgs e)
         {
-            Console.WriteLine("The threshold was reached.");
-            Environment.Exit(0);
+            Console.WriteLine("Obbiettivo raggiunto"); // Output
+            Environment.Exit(0); // Fa terminare il programma con il codice '0'
         }
     }
+
     class Counter
     {
         private int threshold;
         private int total;
 
-        public Counter(int passedThreshold)
-        {
-            threshold = passedThreshold;
-        }
+        // Costruttore a cui passo il valore da raggiungere
+        public Counter(int passedThreshold) => threshold = passedThreshold;
 
+        // Metodo per incrementare il valore di a
         public void Add(int x)
         {
-            total += x;
-            if (total >= threshold)
-            {
-                OnThresholdReached(EventArgs.Empty);
-            }
+            total += x; // Incrementa il totale
+            if (total >= threshold) // Se il totale è >= dell'obbiettivo da raggiungere
+                OnThresholdReached(EventArgs.Empty); // Richiama il metodo OnThresholdReached() e gli passa l'argomento EventArgs.Empty
         }
 
+        // Metodo di quando si raggiunge l'obbiettivo
         protected virtual void OnThresholdReached(EventArgs e)
         {
-            EventHandler handler = ThresholdReached;
-            if (handler != null)
-            {
-                handler(this, e);
-            }
+            EventHandler handler = ThresholdReached; // Oggetto EventHandler gli assegna il valore dell'Event della classe
+            if (handler != null) // Se l'Handler è diverso da null
+                handler(this, e); // Gli passa l'istanza, e gli eventi
         }
 
-        public event EventHandler ThresholdReached;
+        public event EventHandler ThresholdReached; // EvenetHandler
     }
 }
